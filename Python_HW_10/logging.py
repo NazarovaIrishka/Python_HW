e7f8706f05d288f58a69d2753041fe280472fe62
Irina <marimashka96@mail.ru>
2022-10-23 20:01:37 +0300

    Save 2.0

--- Python_HW_10/logging.py
+++ Python_HW_10/logging.py
@@ -5,5 +5,5 @@ import datetime
 def log (update: Update, context: CallbackContext):
     mess_time = datetime.datetime.now()
     file = open('act.csv', 'a')
-    file.write(f'{mess_time} {update.effective_user.first_name} : {update.message.text} ')
+    file.write(f'{mess_time} {update.effective_user.first_name} : {update.message.text}\n ')
     file.close()
